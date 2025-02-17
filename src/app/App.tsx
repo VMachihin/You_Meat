00de7ba927189a0c94b51d42b5006a265c1946e0
VitMach <machihin_vitalii@mail.ru>
2025-02-17 23:27:29 +0300

    Начало

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import './styles/index.scss';
+
+export const App = () => {
+  return (
+    <>
+      <h1>Test</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@/',
+        replacement: path.resolve('src') + '/',
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@use "@/styles/helpers" as *;`,
+      },
+    },
+  },
+});
